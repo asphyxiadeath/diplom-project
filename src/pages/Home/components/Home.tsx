a55730cbad890ad0133fea628d2354c928532a4d
asphyxiadeath <86077497+asphyxiadeath@users.noreply.github.com>
2021-12-19 19:02:26 +0300

    create new companent

--- src/pages/Home/components/Home.tsx
+++ src/pages/Home/components/Home.tsx
@@ -1,10 +1,19 @@
-import ts from "typescript";
+import React from 'react';
 import style from './Home.module.scss';
+import { ThisDay } from './ThisDay/ThisDay';
+import {ThisDayInfo} from './ThisDayInfo/ThisDayInfo';
 
 interface Props {
 
 }
 
 export const Home = (props: Props) => {
-    return  <div className={style.Home}></div>;
+    return (
+    <div className={style.Home}>
+        <div className={style.wrapper}>
+        <ThisDay />
+        <ThisDayInfo />
+        </div>
+    </div>
+    );
 };
---
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import style from './ThisDay.module.scss';
+import { GlobalSvgSelector } from '../../../../assets/Images/icons/global/GlobalSvgSelector';
+
+interface Props {}
+
+export const ThisDay = (props: Props) => {
+    return (
+    <div className={style.this_day}>
+         <div className={style.top_block}>
+            <div className={style.top_block_wrapper}>
+                <div className={style.this_temp}>20°</div>
+                <div className={style.this_day_name}>Сегодня</div>
+            </div>
+            <GlobalSvgSelector id="sun" />
+            </div>
+         <div className={style.bottom_block}>
+            <div className={style.this_time}>
+                 Время: <span>21:54</span>
+            </div>
+            <div className={style.this_city}>
+                Время: <span>Минск</span>
+        </div>
+      </div>
+    </div>
+    );
+        
+    };
